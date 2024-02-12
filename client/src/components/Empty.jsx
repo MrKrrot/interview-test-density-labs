e83cdf3a3140703eba8fef943462a306875fe04b
MrKrrot <rafita.olguin@hotmail.com>
2024-02-11 19:35:03 -0600

    style: :lipstick: update the styles for empty comments section

---
+++ client/src/components/Empty.jsx
@@ -0,0 +1,7 @@
+export const Empty = () => {
+  return (
+    <div className="flex items-center justify-center h-64">
+      <p className="text-white text-2xl">No comments yet. Be the first!</p>
+    </div>
+  )
+}
--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,5 +1,5 @@
 import { useEffect } from 'react'
-import { CommentForm, CommentList } from '@components'
+import { CommentForm, CommentList, Empty } from '@components'
 import { useComments } from '@hooks'
 
 const HomePage = () => {
@@ -14,7 +14,7 @@ const HomePage = () => {
       <div className="flex flex-col items-center justify-center">
         <h1 className="text-3xl font-bold p-4">Interview Test - Comments</h1>
         <CommentForm />
-        {comments.length > 0 ? <CommentList comments={comments} /> : <p>No comments</p>}
+        {comments.length > 0 ? <CommentList comments={comments} /> : <Empty />}
       </div>
     </main>
   )
